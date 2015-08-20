5c3252494afce9b66142586fdd3ba934c0e19c64
Darwin Groskleg <darwingroskleg@gmail.com>
2015-08-20 01:34:34 -0400

    Adds basic test skeletons for the expected classes.

--- spec/make_my_day_spec.rb
+++ spec/make_my_day_spec.rb
@@ -1,7 +1,56 @@
 require 'spec_helper'
 
 describe MakeMyDay do
-  it 'does tings' do
-    pending # no code yet
+  describe '#' do
+    it 'be' do
+      puts 'Sugar, a lot of sugar'
+      # setup
+      # exercise
+      # verify
+    end
+  end
+end
+
+describe Session do
+  describe '#' do
+    it 'be' do
+      puts 'Be on time'
+      # setup
+      # exercise
+      # verify
+    end
+  end
+end
+
+describe Button do
+  describe '#' do
+    it 'be' do
+      puts 'I gotsta know'
+      # setup
+      # exercise
+      # verify
+    end
+  end
+end
+
+describe Spreadsheet do
+  describe '#' do
+    it 'be' do
+      puts 'Did he fire 6 shots or only 5'
+      # setup
+      # exercise
+      # verify
+    end
+  end
+end
+
+describe Calendar do
+  describe '#' do
+    it 'be' do
+      puts 'Go ahead, make it'
+      # setup
+      # exercise
+      # verify
+    end
   end
 end
