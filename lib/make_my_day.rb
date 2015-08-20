05b18d3c77dae476e55386d35a4f9ff5b824f06e
Darwin Groskleg <darwingroskleg@gmail.com>
2015-08-20 02:12:46 -0400

    Adds empty classes to be GREEN.

--- lib/make_my_day.rb
+++ lib/make_my_day.rb
@@ -3,3 +3,19 @@ require "make_my_day/version"
 module MakeMyDay
   # Your code goes here...
 end
+
+class Session
+  # calendar item
+end
+
+class Button
+  # html link for a session
+end
+
+class Spreadsheet
+  # input file containing session records
+end
+
+class Calendar
+  # output data destination
+end
